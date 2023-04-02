849a9d5fe1abcff8bfee0a8da112ba9d9c382dcb
Carlos-ag <carlosmartindeargila@outlook.es>
2023-04-03 00:01:17 +0200

    ya

--- stockify-api/src/main/java/com/stockify/stockifyapp/commons/Converter.java
+++ stockify-api/src/main/java/com/stockify/stockifyapp/commons/Converter.java
@@ -1,7 +1,5 @@
 package com.stockify.stockifyapp.commons;
 
-import java.io.Console;
-import java.math.BigInteger;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
